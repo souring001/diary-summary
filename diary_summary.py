82eb7b0ef354b3ff5b5ab6817a8994634e50ecc3
aso <titechei@gmail.com>
2020-05-20 22:57:32 +0900

    update

--- diary_summary.py
+++ diary_summary.py
@@ -16,7 +16,8 @@ stop_words = [ u'てる', u'いる', u'なる', u'れる', u'する', u'ある',
              u'くれる', u'やる', u'くださる', u'そう', u'せる', u'した',  u'思う',  \
              u'それ', u'ここ', u'ちゃん', u'くん', u'', u'て',u'に',u'を',u'は',u'の', u'が', u'と', u'た', u'し', u'で', \
              u'ない', u'も', u'な', u'い', u'か', u'ので', u'よう', u'あり', u'ため', u'この', u'なり', u'しまっ', \
-             u'なっ', u'いっ', u'どう', u'たち', u'いく', u'こう', u'その', u'やっ', u'あっ', u'でき', u'いき', u'より']
+             u'なっ', u'いっ', u'どう', u'たち', u'いく', u'こう', u'その', u'やっ', u'あっ', u'でき', u'いき',\
+             u'より', u'いき', u'すん', u'かかっ', u'しよ', u'もん', u'あと', u'すぎ', u'さす', u'くれ']
 
 #助詞、助動詞を除いて単語結合
 splitted = ' '.join([x.split('\t')[0] for x in parsed.splitlines()[:-1] if x.split('\t')[1].split(',')[0] not in ['助詞', '助動詞']])
@@ -29,5 +30,6 @@ wordc = wordcloud.WordCloud(
         contour_color='steelblue',
         width=800,
         height=600,
+        # regexp="[\w']+",
         stopwords=set(stop_words)).generate(splitted)
-wordc.to_file('summary-202004.png')
+wordc.to_file('summary.png')

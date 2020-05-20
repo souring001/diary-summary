9a8f52671c59121ff1e5bb9f29546ca26ade579b
aso <titechei@gmail.com>
2020-05-20 21:07:54 +0900

    stopwords

--- diary_summary.py
+++ diary_summary.py
@@ -11,6 +11,13 @@ m = MeCab.Tagger('')
 text = text.replace('\r','')
 parsed = m.parse(text)
 
+# ストップワードの設定
+stop_words = [ u'てる', u'いる', u'なる', u'れる', u'する', u'ある', u'こと', u'これ', u'さん', u'して', \
+             u'くれる', u'やる', u'くださる', u'そう', u'せる', u'した',  u'思う',  \
+             u'それ', u'ここ', u'ちゃん', u'くん', u'', u'て',u'に',u'を',u'は',u'の', u'が', u'と', u'た', u'し', u'で', \
+             u'ない', u'も', u'な', u'い', u'か', u'ので', u'よう', u'あり', u'ため', u'この', u'なり', u'しまっ', \
+             u'なっ', u'いっ', u'どう', u'たち', u'いく', u'こう', u'その', u'やっ', u'あっ', u'でき', u'いき', u'より']
+
 #助詞、助動詞を除いて単語結合
 splitted = ' '.join([x.split('\t')[0] for x in parsed.splitlines()[:-1] if x.split('\t')[1].split(',')[0] not in ['助詞', '助動詞']])
 
@@ -18,8 +25,9 @@ wordc = wordcloud.WordCloud(
         font_path='RiiTN_R.otf',
         background_color='white',
         # mask=msk,
+        # contour_width=4,
         contour_color='steelblue',
         width=800,
         height=600,
-        contour_width=2).generate(splitted)
+        stopwords=set(stop_words)).generate(splitted)
 wordc.to_file('summary-202004.png')
